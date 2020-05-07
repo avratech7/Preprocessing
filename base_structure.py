6fd64e4ffca9e91f99dee70eb8a3d71c710bd744
Betzalel Kenig <betzalelkenig@gmail.com>
2020-05-07 08:17:05 +0300

    add base_structure file

---
+++ base_structure.py
@@ -0,0 +1,27 @@
+
+
+class Preprocessing():
+
+	def split_to_paragraphs(original_text):
+		pass
+
+
+	def remove_not_letters():
+		pass
+
+
+	'''
+		Remove "Stop words" like "the", "on", "is". 
+	'''
+	def remove_stop_words():
+		#from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+		pass
+
+
+	'''
+		Stemming is a process of reducing words to their word stem, base or root form
+	'''
+	def stemming_the_paragraphs():
+		#https://tartarus.org/martin/PorterStemmer/
+		#https://web.archive.org/web/20140827005744/http:/www.comp.lancs.ac.uk/computing/research/stemming/index.htm
+		pass
